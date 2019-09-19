ff6fc286b3cb88c1ce122b7aee315fb852adcacc
donmahallem <donmahallem@users.noreply.github.com>
2019-09-19 19:33:16 +0200

    Bump

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import * as github from '@actions/github';
+import * as actionscore from '@actions/core';
+
 console.log("test");
 const payload = JSON.stringify(github.context.payload, undefined, 2);
 console.log(payload);
+actionscore.setOutput("T1", "T2");
+actionscore.setFailed("Bommel");
