679e2b67a8c9d3013a5b702e23d020810a644224
donmahallem <donmahallem@users.noreply.github.com>
2019-09-21 10:00:35 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
 import * as github from '@actions/github';
 import * as actionscore from '@actions/core';
+import { Config } from './config';
 
-console.log("test");
-const payload = JSON.stringify(github.context.payload, undefined, 2);
-console.log(payload);
-actionscore.setOutput("T1", "T2");
-actionscore.setFailed("Bommel");
+const config: Config = {
+    GITHUB_SECRET: actionscore.getInput("github_secret", {
+        required: true
+    })
+};
+
+if (github.context.payload.pull_request) {
+    const prData: any = github.context.payload.pull_request;
+    if (prData.labels && prData.labels.length > 0 && config) {
+        actionscore.info("Label present");
+    } else {
+        actionscore.setFailed("No Label");
+    }
+} else {
+
+    console.log("test");
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(payload);
+    actionscore.setOutput("T1", "T2");
+    actionscore.setFailed("Bommel");
+}
